d2217bd871ebe1fb70238b581940f82b6533686a
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-02-08 21:05:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "vpc" {
   source = "./vpc_module"
 }
