c41ac704cde7513fb261f076143556283ae842eb
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-02-08 21:33:27 -0500

    Update ec2.tf

--- ec2_instance_module/ec2.tf
+++ ec2_instance_module/ec2.tf
@@ -1,6 +1,8 @@
-variable "vpc_id" {}
+variable "vpc_id" {
+}
 
-variable "subnet_id" {}
+variable "subnet_id" {
+}
 
 resource "aws_instance" "example" {
   ami           = "ami-12345678"  # Replace with your AMI ID
